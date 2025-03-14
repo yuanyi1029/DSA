58b26727e54d69b34301eba0e40a2ca176f0e84a
ywon0095 <ywon0095@student.monash.edu>
2025-03-14 15:53:21 +0800

    z-algorithm documentation

--- algorithms/pattern_matching/z_algorithm.py
+++ algorithms/pattern_matching/z_algorithm.py
@@ -8,16 +8,21 @@ def z_algorithm(sentence):
 
     while index < len(sentence): 
         # Case 2: index is within a z-box
+        # Resolve: Resolution is split into 3 cases 
         if index >= left and index <= right: 
-            # Case 2a: 
+
+            # Case 2a: corresponding z-value is within z-box
+            # Resolve: use the corresponding z-value 
             if z_array[index - left] < right - index + 1:  
                 z_array[index] = z_array[index - left]
 
-            # Case 2b: 
+            # Case 2b: corresponding z-value excludes z-box
+            # Resolve: use the remaining space between index and right as z-value
             elif z_array[index - left] > right - index + 1:  
                 z_array[index] = right - index + 1 
 
-            # Case 2c: 
+            # Case 2c: corresponding z-value is equal to the z-box
+            # Resolve: perform manual comparison from right onwards 
             else:
                 start = right - index + 2 
                 current = right + 1   
@@ -35,6 +40,7 @@ def z_algorithm(sentence):
                     right = current - 1
 
         # Case 1: index is not within a z-box
+        # Resolve: perform manual comparison 
         else: 
             start = 0 
             current = index
